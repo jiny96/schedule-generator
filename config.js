a3db50993f8d6f0e90bafaa54151d66dfef2001a
jiny96 <dlaaudwlssk@gmail.com>
2026-05-26 16:38:48 +0900

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ window.SCHEDULE_CONFIG = {
 
   // Google Apps Script 웹앱 배포 URL
   // 형식: https://script.google.com/macros/s/XXXXXX/exec
-  APPS_SCRIPT_URL: 'YOUR_APPS_SCRIPT_URL_HERE',
+  APPS_SCRIPT_URL: 'https://script.google.com/a/macros/neowiz.com/s/AKfycbyj6dbLqMDwRARUZTdlnrTrGOpved6n1LquYs4awPw-_s0zUHrbq-IjBZNZRZ8carCLLw/exec',
 
 };
